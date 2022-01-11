c599745d9bcb23eb0cc869a7e698f251f46d9b79
JH Ms <jhms@JHs-MacBook-Air.local>
2022-01-11 17:41:03 +0900

    Add: prodjcts application

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'NAMES',
+        'USER' : 'root',
+        'PASSWORD' : 'youngforever',
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306'
+    }
+}
+
+SECRET_KEY = 'django-insecure-oe1pr64zz(t3+5$=39+1)=-%z%v-x%#h-7_1q$5)4pp=lpqi6l'
